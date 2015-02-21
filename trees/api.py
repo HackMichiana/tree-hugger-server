cbe0c5db2d501a6dbcbc2123eed41474985aaa74
Nathan Smith <nathanrandal@gmail.com>
2015-02-21 14:21:20 -0500

    added open auth to trees api resource

--- trees/api.py
+++ trees/api.py
@@ -1,8 +1,11 @@
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 from trees.models import Tree
 
 
 class TreeResource(ModelResource):
     class Meta:
+        authorization = Authorization()
+        allowed_methods = ['get', 'post']
         queryset = Tree.objects.all()
         resource_name = 'tree'
