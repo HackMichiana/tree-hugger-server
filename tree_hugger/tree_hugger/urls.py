0b8b5f507e62be23379d29033fa8a0974f97af53
Collin Anderson <cmawebsite@gmail.com>
2015-02-21 10:24:07 -0500

    removed deprecated, unneeded patterns()

--- tree_hugger/tree_hugger/urls.py
+++ tree_hugger/tree_hugger/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from tastypie.api import Api
 
@@ -7,11 +7,11 @@ import trees.api as treesapi
 api = Api(api_name='v1')
 api.register(treesapi.TreeResource())
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'tree_hugger.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api.urls))
-)
+]
