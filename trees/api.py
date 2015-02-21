6563747cdd9a57e34cc3f31d03d117d1ba84a1ff
Nathan Smith <nathanrandal@gmail.com>
2015-02-21 16:01:57 -0500

    disabled authentication

--- trees/api.py
+++ trees/api.py
@@ -1,11 +1,13 @@
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
+from tastypie.authentication import Authentication
 from trees.models import Tree
 
 
 class TreeResource(ModelResource):
     class Meta:
         authorization = Authorization()
+        authentication = Authentication()
         allowed_methods = ['get', 'post']
         queryset = Tree.objects.all()
         resource_name = 'tree'
