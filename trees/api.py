7da093906ed058340b9797ea795766cf54cb3913
Nathan Smith <nathanrandal@gmail.com>
2015-03-10 00:07:05 -0400

    added some support for CORS access, needs work

--- trees/api.py
+++ trees/api.py
@@ -4,20 +4,59 @@ from tastypie.authorization import Authorization
 from tastypie.authentication import Authentication
 from trees.models import Tree, Image
 
+from string import upper
+from django.http import HttpResponse
+from tastypie.exceptions import ImmediateHttpResponse
+from tastypie import http
 
-class ImageResource(ModelResource):
+
+import pdb
+
+
+class CORSResource(object):
+    """
+    Adds CORS headers to resources that subclass this.
+    """
+    def create_response(self, *args, **kwargs):
+        response = super(CORSResource, self).create_response(*args, **kwargs)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Headers'] = 'accept, authorization, content-type'
+        return response
+
+    def method_check(self, request, allowed=None):
+        if allowed is None:
+            allowed = []
+
+        request_method = request.method.lower()
+        allows = ','.join(map(str.upper, allowed))
+
+        if request_method == 'options':
+            response = HttpResponse(allows)
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Headers'] = 'accept, authorization, content-type'
+            response['Allow'] = allows
+            raise ImmediateHttpResponse(response=response)
+
+        if not request_method in allowed:
+            response = http.HttpMethodNotAllowed(allows)
+            response['Allow'] = allows
+            raise ImmediateHttpResponse(response=response)
+
+        return request_method
+
+class ImageResource(CORSResource, ModelResource):
     class Meta:
         authorization = Authorization()
         authentication = Authentication()
-        allowed_methods = ['get']
+        allowed_methods = ['get','options']
         queryset = Image.objects.all()
         resource_name = 'image'
 
-class TreeResource(ModelResource):
+class TreeResource(CORSResource, ModelResource):
     images = fields.ToManyField(ImageResource, 'image_set', related_name='tree', full=True, null=True)
     class Meta:
         authorization = Authorization()
         authentication = Authentication()
-        allowed_methods = ['get', 'post']
+        allowed_methods = ['get', 'post','options']
         queryset = Tree.objects.all()
         resource_name = 'tree'
